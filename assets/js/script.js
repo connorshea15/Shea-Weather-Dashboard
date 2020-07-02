075d0a33181a92ba113086d8af4791eb05716feb
connorshea15 <connorshea15@gmail.com>
2020-07-02 14:31:18 -0700

    API call but I cannot test it because it takes a while for my api key to start working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
 // This is the javascript file
+
+// I need to wait on this to see if my shiz becomes active later
+var getWeather = function() {
+    apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=229c1683379f39bf8bd6307cd867b979";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    });
+};
